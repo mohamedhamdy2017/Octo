8f678484e1313eff0d861405736871ad60f90891
mohamedhamdy2017 <mohamedhamdy3482004@gmail.com>
2023-08-15 01:36:39 +0300

    add new images

---
+++ src/Images/index.ts
@@ -0,0 +1,11 @@
+const IMAGES = {
+  carswitch: require('../Images/carswitch.png'),
+  location: require('../Images/location.png'),
+  logo: require('../Images/logo.png'),
+  notification: require('../Images/notification.png'),
+  search: require('../Images/search.png'),
+  odometer: require('../Images/odometer.png'),
+  calendar: require('../Images/calendar.png'),
+  stars: require('../Images/stars.png'),
+};
+export {IMAGES};
